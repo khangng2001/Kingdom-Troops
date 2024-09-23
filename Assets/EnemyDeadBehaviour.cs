6368b0adff57a2f7cd7e7d5e86803d88348e2ba2
MinhThu <leminhthu250203@gmail.com>
2024-09-23 10:52:21 +0700

    update

--- Assets/EnemyDeadBehaviour.cs
+++ Assets/EnemyDeadBehaviour.cs
@@ -19,7 +19,7 @@ public class EnemyDeadBehaviour : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-		animator.gameObject.SetActive(false);
+		//animator.gameObject.SetActive(false);
 		animator.gameObject.GetComponent<PlayerController>().ShowYouDie();
     }
 
